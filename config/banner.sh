87046074e3696d273a9964ac40f14678547e05f9
Seaton Jiang <hi@seatonjiang.com>
2024-01-06 12:34:10 +0800

    feat: add the default config

---
+++ config/banner.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Narsil (Rocky Linux) - Security hardening tool
+# Seaton Jiang <hi@seatonjiang.com>
+#
+# The latest version of Narsil can be found at:
+# https://github.com/seatonjiang/narsil-rockylinux
+#
+# Licensed under the MIT license:
+# https://github.com/seatonjiang/narsil-rockylinux/blob/main/LICENSE
+#
+
+/etc/banner/00-narsil-header
+/etc/banner/10-narsil-system-info
+/etc/banner/11-narsil-disk-usage
+/etc/banner/12-narsil-docker-status
